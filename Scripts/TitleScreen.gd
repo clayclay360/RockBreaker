9ed4146748e6ef93e4f3c2752125d470f50076ed
Clay <38699310+clayclay360@users.noreply.github.com>
2021-04-11 16:45:37 -0400

    Update TitleScreen.gd

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -5,8 +5,8 @@ var audio_time
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$GameplayAudio.play(GameState.audio_time)
-	$HighScoreLabel.text = "high score: " + String(GameState.high_score) + "s"
+	$GameplayAudio.play(GameState.audio_time) # player game player audio
+	$HighScoreLabel.text = "high score: " + String(GameState.high_score) + "s" # display the high score
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,6 +15,6 @@ func _ready():
 
 
 func _on_TextureButton_pressed():
-	GameState.audio_time = $GameplayAudio.get_playback_position()
-	get_tree().change_scene("res://Scenes/Main.tscn")
+	GameState.audio_time = $GameplayAudio.get_playback_position() # set the audio time equal to the game play audio's position
+	get_tree().change_scene("res://Scenes/Main.tscn") # load the main scene 
 	
