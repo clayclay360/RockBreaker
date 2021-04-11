97e9b85b28e6b0e9001cf88546a06d3fd6377e3c
Clay <38699310+clayclay360@users.noreply.github.com>
2021-04-11 12:46:53 -0400

    Update Bullet.gd

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -3,27 +3,28 @@ extends Area2D
 export (int) var speed
 var velocity = Vector2()
 
+# this function will be called when the bullet is instanced, get the starting position, rotation and speed of the bullet
 func start(pos, dir):
 	position = pos
 	rotation = dir
 	velocity = Vector2(speed,0).rotated(dir)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# during every frame increase the position by the velocity multiplied by delta
 func _process(delta):
 	position += velocity * delta
 
-
+# if the bullet goes outside the screen view, delete the bullet
 func _on_VisibilityNotifier2D_viewport_exited(viewport):
 	queue_free()
 
-
+# call this function if the bullet enters another nodes body
 func _on_Bullet_body_entered(body):
 	if body.is_in_group("rocks"):
-		body.explode()
-		queue_free()
+		body.explode() # run the explode function
+		queue_free() # destory bullet
 
-
+# call this function if the bullet enters another nodes area
 func _on_Bullet_area_entered(area):
 	if area.is_in_group("enemies"):
-		area.take_damage(1)
-		queue_free()
+		area.take_damage(1) # run the take_damage function and pass in the argument 1
+		queue_free() # destroy bullet
