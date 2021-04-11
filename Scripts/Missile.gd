9f3ffec8e5af3e70a89e6621929028ea3a6e5611
Clay <38699310+clayclay360@users.noreply.github.com>
2021-04-11 16:01:37 -0400

    Update Missile.gd

--- Scripts/Missile.gd
+++ Scripts/Missile.gd
@@ -8,43 +8,43 @@ var velocity = Vector2()
 var acceleration = Vector2()
 
 func start(pos, dir, target):
-	position = pos
-	rotation = dir
-	area = target
-	velocity = Vector2(speed,0).rotated(dir)
-	get_corrdinate()
+	position = pos # position equals pos
+	rotation = dir # rotation equals dir 
+	area = target # area equals target
+	velocity = Vector2(speed,0).rotated(dir) # velocity equals speed along the x axis, rotated by the direction
+	get_corrdinate() # run get_coordinate 
 
 func _physics_process(delta):
-	position += velocity * delta
+	position += velocity * delta # increase the position by the velocity multiplied by delta
 	#rotation = velocity.angle() * delta
 	if area != null:
-		lock_on()
+		lock_on() # run lock_on
 	
 func get_corrdinate():
 	if area != null:
-		area_position = area.position
+		area_position = area.position # equal the area's position
 		
 func lock_on():
-	dir = area.position - position
-	dir.normalized()
-	acceleration += (dir-velocity).normalized()
-	velocity += acceleration
-	rotation = velocity.angle()
-	#rotation = -dir.cross(transform.x) * 100
+	dir = area.position - position # have dir equal the area's position subtracted by it's (itself, the missile) position
+	dir.normalized() # normalize the vector2
+	acceleration += (dir-velocity).normalized() # increase the acceleration by dir minus velocity and get that vector's normalization
+	velocity += acceleration # velocity plus equals acceleration
+	rotation = velocity.angle() # rotation equals the velocity's angle
+	#rotation = -dir.cross(transform.x) * 100 
 	
 func _on_Timer_timeout():
-	queue_free()
+	queue_free() # destroy missile
 
 func _on_Missile_body_entered(body):
 	if body.is_in_group("rocks"):
-		body.explode()
-		queue_free()
+		body.explode() # run the explode function
+		queue_free() # destroy missile
 	
 func _on_VisibilityNotifier2D_viewport_exited(viewport):
-	queue_free()
+	queue_free() # destroy missile
 
 
 func _on_Missile_area_entered(area):
 	if area.is_in_group("enemies"):
-		area.take_damage(3)
-		queue_free()
+		area.take_damage(3) # run take_damage with the agrument of 3
+		queue_free() # destroy missile
