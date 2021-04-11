92d02293c30f0e52d10a4f76f49175a7d12c6e37
Clay <38699310+clayclay360@users.noreply.github.com>
2021-04-11 16:49:03 -0400

    Update Trial.gd

--- Scripts/Trial.gd
+++ Scripts/Trial.gd
@@ -7,12 +7,12 @@ var point
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_as_toplevel(true)
+	set_as_toplevel(true) # top level to true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	point = get_parent().global_position
-	add_point(point)
+	point = get_parent().global_position # set point to parent's global position
+	add_point(point) # run the add point function with the agrument point
 	if points.size() > 10:
-		remove_point(0)
+		remove_point(0) # remove the first point
